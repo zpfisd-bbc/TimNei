372f8391f2e34b2210927c90fba34a1e2fbf1b3e
Dominic Pfister <dominic.pfister@swissonline.ch>
2015-04-01 22:50:48 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Tim nei...</title>
+        <!-- ** CSS ** -->
+        <link rel="stylesheet" type="text/css" href="css/theme.css" />
+    </head>
+    <body>
+    <!-- ** PHP ** -->
+    
+    <!-- ** HTML ** -->
+    <div class="centered">
+    	<a href="#" class="btn btn-primary timbtn">tim nei</a>
+    </div>
+    </body>
+</html>
