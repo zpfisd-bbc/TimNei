5462eb87cbd8593a146dfa51b0b86c604f88f704
Dominic Pfister <dominic.pfister@bbcag.ch>
2015-04-02 09:58:50 +0200

    V0.1
    - zwei buttons

--- index.php
+++ index.php
@@ -4,13 +4,15 @@
         <title>Tim nei...</title>
         <!-- ** CSS ** -->
         <link rel="stylesheet" type="text/css" href="css/theme.css" />
+        <link rel="stylesheet" type="text/css" href="css/hover.css" />
     </head>
     <body>
     <!-- ** PHP ** -->
     
     <!-- ** HTML ** -->
     <div class="centered">
-    	<a href="#" class="btn btn-primary timbtn">tim nei</a>
+    	<a href="#" class="btn btn-danger timbtn hvr-fade-red">tim nei</a>
+    	<a href="#" class="btn btn-warning timbtn2 hvr-fade-orange">tim ja</a>
     </div>
     </body>
 </html>
