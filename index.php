ef23a92659b794597b0ac09e9d910e23de859c7d
Dominic Pfister <dominic.pfister@bbcag.ch>
2015-04-02 12:42:35 +0200

    Zusammenfügung

--- index.php
+++ index.php
@@ -10,8 +10,6 @@
 	<!-- ** PHP ** -->
     <!-- ** HTML ** -->
 	<div class="centered">
-
-
 		<form id="timnei" action="resources/nei_counter.php" method="post">
 			<input type="submit" value="tim nei"
 				class="btn btn-danger timbtn hvr-fade-red" />
@@ -21,8 +19,38 @@
 			<input type="submit" value="tim ja"
 				class="btn btn-warning timbtn2 hvr-fade-orange" />
 		</form>
-
-
 	</div>
-</body>
+		<div class="panel panel-danger counter">
+	  <div class="panel-heading">
+	    <h3 class="panel-title">Tim Nei</h3>
+	  </div>
+	  <div class="panel-body">
+	  	0
+	  </div>
+	</div>
+	
+	<div class="panel panel-warning counter">
+	  <div class="panel-heading">
+	    <h3 class="panel-title">Tim Ja</h3>
+	  </div>
+	  <div class="panel-body">
+	  	0
+	  </div>
+	</div>
+	
+	<div class="progress progress-striped active counterProgress">
+  		<div class="progress-bar progress-bar-danger" style="width: 80%"></div>
+		<div class="progress-bar progress-bar-warning" style="width: 20%"></div>
+	</div>
+	
+    </body>
+    
+    <footer>
+    <div class="panel-footer">
+    	<hr>
+    	<h5></h5>
+    	<h5>&#169; TimNei</h5>
+    </div>
+    </footer>
+    
 </html>
